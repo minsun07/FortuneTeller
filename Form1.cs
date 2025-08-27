3c0cc9db06b3faeac0dfbdc62f62abb0e7afbd27
Minsun An <elleto123456@gmail.com>
2025-08-27 09:19:43 +0900

    #5 LoadResults 매서드 구현

--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,7 +17,33 @@ namespace FortuneTeller
         public Form1()
         {
             InitializeComponent();
+            LoadResults();
+        }
 
+        private void LoadResults()
+        {
+            try
+            {
+                string filename = "results.csv";
+                results = File.ReadAllLines(filename).ToList();
+            }
+            catch(FileNotFoundException ex)
+            {
+                MessageBox.Show($"파일을 불러올 수 없습니다.\n{ex.Message}", "파일 없음",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch(UnauthorizedAccessException ex)
+            {
+                MessageBox.Show($"파일에 접근권한이 없습니다.\n{ex.Message}", "권한 문제",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show($"알 수 없는 오류가 발생했어요.\n{ex.Message}", "알 수 없는 오류!",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                
+            }
+            
         }
 
         private void Form1_Load(object sender, EventArgs e)
